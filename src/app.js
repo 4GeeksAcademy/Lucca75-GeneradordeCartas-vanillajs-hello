af72af13cfba04d41640b53b9873fe20d66c4ddb
Luccadz75 <149024268+Luccadz75@users.noreply.github.com>
2024-01-08 03:30:00 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateRandomCard() {
+  const suits = ["♥", "♦", "♣", "♠"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const card = document.getElementById("card");
+  card.className = "card";
+
+  const center = document.createElement("div");
+  center.className = "suit center";
+  center.innerHTML = `<div class="value">${randomValue}</div>`;
+  card.appendChild(center);
+
+  const upperLeft = document.createElement("div");
+  upperLeft.className = "suit upper-left";
+  upperLeft.innerHTML = randomSuit;
+  card.appendChild(upperLeft);
+
+  const lowerRight = document.createElement("div");
+  lowerRight.className = "suit lower-right";
+  lowerRight.innerHTML = randomSuit;
+  card.appendChild(lowerRight);
+}
